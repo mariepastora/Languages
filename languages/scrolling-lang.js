82c9464cf2b066b97318d27405b9bd220f447901
mariepastora <mpp2134@columbia.edu>
2018-09-23 18:20:29 -0400

    Adding new version of the languages folder

---
+++ languages/scrolling-lang.js
@@ -0,0 +1,311 @@
+d3.select('#step-zero-astoria').on('stepin', function() {
+  console.log('I was stepped into!')
+  div = document.getElementById('text-astoria')
+  div.style.color = 'transparent'
+  english = document.getElementById('english_astoria')
+  english.style.backgroundColor = 'rgba(146, 208, 203, 0.5)'
+  french = document.getElementById('french_astoria')
+  french.style.backgroundColor = 'rgba(20,101,5, 0.5)'
+  polish = document.getElementById('polish_astoria')
+  polish.style.backgroundColor = 'rgba(237,164,253,0.5)'
+  russian = document.getElementById('russian_astoria')
+  russian.style.backgroundColor = 'rgba(242, 165, 144, 0.5)'
+  hungarian = document.getElementById('hungarian_astoria')
+  hungarian.style.backgroundColor = 'rgba(38,52,1,0.5)'
+  spanish = document.getElementById('spanish_astoria')
+  spanish.style.backgroundColor = 'rgba(244, 176, 103, 0.5)'
+  portuguese = document.getElementById('portuguese_astoria')
+  portuguese.style.backgroundColor = 'rgba(150,39,58,0.5)'
+  greek = document.getElementById('greek_astoria')
+  greek.style.backgroundColor = 'rgba(8,25,103, 0.5)'
+  croatian = document.getElementById('croatian_astoria')
+  croatian.style.backgroundColor = 'rgba(46,125,181, 0.5)'
+  others = document.getElementById('others_astoria')
+  others.style.backgroundColor = 'rgba(134, 137, 152, 0.5)'
+  arabic = document.getElementById('arabic_astoria')
+  arabic.style.backgroundColor = 'rgba(234, 105, 175, 0.5)'
+  chinese = document.getElementById('chinese_astoria')
+  chinese.style.backgroundColor = 'rgba(209,245,35, 0.5)'
+  japanese = document.getElementById('japanese_astoria')
+  japanese.style.backgroundColor = 'rgba(209,245,35, 0.5)'
+  urdu = document.getElementById('urdu_astoria')
+  urdu.style.backgroundColor = 'rgba(3,117,251,0.5)'
+  korean = document.getElementById('korean_astoria')
+  korean.style.backgroundColor = 'rgba(106,130,108,0.5)'
+  tagalog = document.getElementById('tagalog_astoria')
+  tagalog.style.backgroundColor = 'rgba(164,41,33,0.5)'
+})
+
+d3.select('#step-one-astoria').on('stepin', function() {
+  console.log('I was stepped into!')
+  div = document.getElementById('text-astoria')
+  div.style.color = '#282828'
+  english = document.getElementById('english_astoria')
+  english.style.backgroundColor = 'rgba(146, 208, 203, 0.2)'
+  french = document.getElementById('french_astoria')
+  french.style.backgroundColor = 'rgba(20,101,5, 0.2)'
+  polish = document.getElementById('polish_astoria')
+  polish.style.backgroundColor = 'rgba(237,164,253,0.2)'
+  russian = document.getElementById('russian_astoria')
+  russian.style.backgroundColor = 'rgba(242, 165, 144, 0.2)'
+  hungarian = document.getElementById('hungarian_astoria')
+  hungarian.style.backgroundColor = 'rgba(38,52,1,0.2)'
+  spanish = document.getElementById('spanish_astoria')
+  spanish.style.backgroundColor = 'rgba(244, 176, 103, 0.2)'
+  portuguese = document.getElementById('portuguese_astoria')
+  portuguese.style.backgroundColor = 'rgba(150,39,58,0.2)'
+  greek = document.getElementById('greek_astoria')
+  greek.style.backgroundColor = 'rgba(8,25,103, 0.2)'
+  croatian = document.getElementById('croatian_astoria')
+  croatian.style.backgroundColor = 'rgba(46,125,181, 0.2)'
+  others = document.getElementById('others_astoria')
+  others.style.backgroundColor = 'rgba(134, 137, 152, 0.2)'
+  arabic = document.getElementById('arabic_astoria')
+  arabic.style.backgroundColor = 'rgba(234, 105, 175, 0.2)'
+  chinese = document.getElementById('chinese_astoria')
+  chinese.style.backgroundColor = 'rgba(209,245,35, 0.2)'
+  japanese = document.getElementById('japanese_astoria')
+  japanese.style.backgroundColor = 'rgba(209,245,35, 0.2)'
+  urdu = document.getElementById('urdu_astoria')
+  urdu.style.backgroundColor = 'rgba(3,117,251,0.2)'
+  korean = document.getElementById('korean_astoria')
+  korean.style.backgroundColor = 'rgba(106,130,108,0.2)'
+  tagalog = document.getElementById('tagalog_astoria')
+  tagalog.style.backgroundColor = 'rgba(164,41,33,0.2)'
+
+  document.getElementById('text-astoria').style.transition = 'all 4s'
+
+})
+
+d3.select('#step-two-astoria').on('stepin', function() {
+  console.log('I was stepped into!')
+  english = document.getElementById('english_astoria')
+  english.style.opacity = '1'
+  french = document.getElementById('french_astoria')
+  french.style.opacity = '1'
+  polish = document.getElementById('polish_astoria')
+  polish.style.opacity = '1'
+  russian = document.getElementById('russian_astoria')
+  russian.style.opacity = '1'
+  hungarian = document.getElementById('hungarian_astoria')
+  hungarian.style.opacity = '1'
+  spanish = document.getElementById('spanish_astoria')
+  spanish.style.opacity = '1'
+  portuguese = document.getElementById('portuguese_astoria')
+  portuguese.style.opacity = '1'
+  greek = document.getElementById('greek_astoria')
+  greek.style.opacity = '1'
+  croatian = document.getElementById('croatian_astoria')
+  croatian.style.opacity = '1'
+  others = document.getElementById('others_astoria')
+  others.style.opacity = '1'
+  arabic = document.getElementById('arabic_astoria')
+  arabic.style.opacity = '1'
+  chinese = document.getElementById('chinese_astoria')
+  chinese.style.opacity = '1'
+  japanese = document.getElementById('japanese_astoria')
+  japanese.style.opacity = '1'
+  urdu = document.getElementById('urdu_astoria')
+  urdu.style.opacity = '1'
+  korean = document.getElementById('korean_astoria')
+  korean.style.opacity = '1'
+  tagalog = document.getElementById('tagalog_astoria')
+  tagalog.style.opacity = '1'
+})
+
+d3.select('#step-three-astoria').on('stepin', function() {
+  console.log('I was stepped into!')
+  english = document.getElementById('english_astoria')
+  english.style.opacity = '0.3'
+  french = document.getElementById('french_astoria')
+  french.style.opacity = '0.3'
+  polish = document.getElementById('polish_astoria')
+  polish.style.opacity = '0.3'
+  russian = document.getElementById('russian_astoria')
+  russian.style.opacity = '0.3'
+  hungarian = document.getElementById('hungarian_astoria')
+  hungarian.style.opacity = '0.3'
+  spanish = document.getElementById('spanish_astoria')
+  spanish.style.opacity = '0.3'
+  portuguese = document.getElementById('portuguese_astoria')
+  portuguese.style.opacity = '0.3'
+  greek = document.getElementById('greek_astoria')
+  greek.style.opacity = '1'
+  croatian = document.getElementById('croatian_astoria')
+  croatian.style.opacity = '0.3'
+  others = document.getElementById('others_astoria')
+  others.style.opacity = '0.3'
+  arabic = document.getElementById('arabic_astoria')
+  arabic.style.opacity = '0.3'
+  chinese = document.getElementById('chinese_astoria')
+  chinese.style.opacity = '0.3'
+  japanese = document.getElementById('japanese_astoria')
+  japanese.style.opacity = '0.3'
+  urdu = document.getElementById('urdu_astoria')
+  urdu.style.opacity = '0.3'
+  korean = document.getElementById('korean_astoria')
+  korean.style.opacity = '0.3'
+  tagalog = document.getElementById('tagalog_astoria')
+  tagalog.style.opacity = '0.3'
+})
+
+d3.select('#step-zero-bp').on('stepin', function() {
+  console.log('I was stepped into!')
+  div = document.getElementById('text-generated')
+  div.style.color = 'transparent'
+  english = document.getElementById('english_boroughPark')
+  english.style.backgroundColor = 'rgba(146, 208, 203, 0.5)'
+  french = document.getElementById('french_boroughPark')
+  french.style.backgroundColor = 'rgba(20,101,5, 0.5)'
+  german = document.getElementById('german_boroughPark')
+  german.style.backgroundColor = 'rgba(252,219,102,0.5)'
+  polish = document.getElementById('polish_boroughPark')
+  polish.style.backgroundColor = 'rgba(237,164,253,0.5)'
+  russian = document.getElementById('russian_boroughPark')
+  russian.style.backgroundColor = 'rgba(242, 165, 144, 0.5)'
+  hungarian = document.getElementById('hungarian_boroughPark')
+  hungarian.style.backgroundColor = 'rgba(38,52,1,0.5)'
+  yiddish = document.getElementById('yiddish_boroughPark')
+  yiddish.style.backgroundColor = 'rgba(49,155,251,0.5)'
+  italian = document.getElementById('italian_boroughPark')
+  italian.style.backgroundColor = 'rgba(36, 74, 251, 0.5)'
+  spanish = document.getElementById('spanish_boroughPark')
+  spanish.style.backgroundColor = 'rgba(244, 176, 103, 0.5)'
+  others = document.getElementById('others_boroughPark')
+  others.style.backgroundColor = 'rgba(134, 137, 152, 0.5)'
+  not_reported = document.getElementById('not_reported_boroughPark')
+  not_reported.style.backgroundColor = 'rgba(134, 137, 152, 0.5)'
+})
+
+d3.select('#step-one-bp').on('stepin', function() {
+  console.log('I was stepped into!')
+  div = document.getElementById('text-generated')
+  div.style.color = '#282828'
+  english = document.getElementById('english_boroughPark')
+  english.style.backgroundColor = 'rgba(146, 208, 203, 0.2)'
+  french = document.getElementById('french_boroughPark')
+  french.style.backgroundColor = 'rgba(20,101,5, 0.2)'
+  german = document.getElementById('german_boroughPark')
+  german.style.backgroundColor = 'rgba(252,219,102,0.2)'
+  polish = document.getElementById('polish_boroughPark')
+  polish.style.backgroundColor = 'rgba(237,164,253,0.2)'
+  russian = document.getElementById('russian_boroughPark')
+  russian.style.backgroundColor = 'rgba(242, 165, 144, 0.2)'
+  hungarian = document.getElementById('hungarian_boroughPark')
+  hungarian.style.backgroundColor = 'rgba(38,52,1,0.2)'
+  yiddish = document.getElementById('yiddish_boroughPark')
+  yiddish.style.backgroundColor = 'rgba(49,155,251,0.2)'
+  italian = document.getElementById('italian_boroughPark')
+  italian.style.backgroundColor = 'rgba(36, 74, 251, 0.2)'
+  spanish = document.getElementById('spanish_boroughPark')
+  spanish.style.backgroundColor = 'rgba(244, 176, 103, 0.2)'
+  others = document.getElementById('others_boroughPark')
+  others.style.backgroundColor = 'rgba(134, 137, 152, 0.2)'
+  not_reported = document.getElementById('not_reported_boroughPark')
+  not_reported.style.backgroundColor = 'rgba(134, 137, 152, 0.2)'
+  english = document.getElementById('english_boroughPark')
+  english.style.opacity = '1'
+  french = document.getElementById('french_boroughPark')
+  french.style.opacity = '1'
+  hungarian = document.getElementById('hungarian_boroughPark')
+  hungarian.style.opacity = '1'
+  italian = document.getElementById('italian_boroughPark')
+  italian.style.opacity = '1'
+  polish = document.getElementById('polish_boroughPark')
+  polish.style.opacity = '1'
+  russian = document.getElementById('russian_boroughPark')
+  russian.style.opacity = '1'
+  spanish = document.getElementById('spanish_boroughPark')
+  spanish.style.opacity = '1'
+  german = document.getElementById('german_boroughPark')
+  german.style.opacity = '1'
+  others = document.getElementById('others_boroughPark')
+  others.style.opacity = '1'
+  not_reported = document.getElementById('not_reported_boroughPark')
+  not_reported.style.opacity = '1'
+  yiddish = document.getElementById('yiddish_boroughPark')
+  yiddish.style.opacity = '1'
+
+  document.getElementById('text-generated').style.transition = 'all 4s'
+})
+
+d3.select('#step-two-bp').on('stepin', function() {
+  console.log('I was stepped into! step 2')
+  english = document.getElementById('english_boroughPark')
+  english.style.opacity = '0.3'
+  french = document.getElementById('french_boroughPark')
+  french.style.opacity = '0.3'
+  hungarian = document.getElementById('hungarian_boroughPark')
+  hungarian.style.opacity = '0.3'
+  italian = document.getElementById('italian_boroughPark')
+  italian.style.opacity = '0.3'
+  polish = document.getElementById('polish_boroughPark')
+  polish.style.opacity = '0.3'
+  russian = document.getElementById('russian_boroughPark')
+  russian.style.opacity = '0.3'
+  spanish = document.getElementById('spanish_boroughPark')
+  spanish.style.opacity = '0.3'
+  german = document.getElementById('german_boroughPark')
+  german.style.opacity = '0.3'
+  others = document.getElementById('others_boroughPark')
+  others.style.opacity = '0.3'
+  not_reported = document.getElementById('not_reported_boroughPark')
+  not_reported.style.opacity = '0.3'
+  yiddish = document.getElementById('yiddish_boroughPark')
+  yiddish.style.opacity = '1'
+
+  document.getElementById('text-generated').style.transition = 'all 4s'
+
+})
+d3.select('#step-three-bp').on('stepin', function() {
+  console.log('I was stepped into! step 2')
+  document.getElementById('text-generated').style.transition = 'all 4s'
+  english = document.getElementById('english_boroughPark')
+  english.style.opacity = '0.3'
+  french = document.getElementById('french_boroughPark')
+  french.style.opacity = '0.3'
+  hungarian = document.getElementById('hungarian_boroughPark')
+  hungarian.style.opacity = '0.3'
+  italian = document.getElementById('italian_boroughPark')
+  italian.style.opacity = '1'
+  polish = document.getElementById('polish_boroughPark')
+  polish.style.opacity = '0.3'
+  russian = document.getElementById('russian_boroughPark')
+  russian.style.opacity = '0.3'
+  spanish = document.getElementById('spanish_boroughPark')
+  spanish.style.opacity = '0.3'
+  german = document.getElementById('german_boroughPark')
+  german.style.opacity = '0.3'
+  others = document.getElementById('others_boroughPark')
+  others.style.opacity = '0.3'
+  not_reported = document.getElementById('not_reported_boroughPark')
+  not_reported.style.opacity = '0.3'
+  yiddish = document.getElementById('yiddish_boroughPark')
+  yiddish.style.opacity = '0.3'
+})
+
+d3.select('#step-four-bp').on('stepin', function() {
+  console.log('I was stepped out of, whatever')
+  english = document.getElementById('english_boroughPark')
+  english.style.opacity = '1'
+  french = document.getElementById('french_boroughPark')
+  french.style.opacity = '1'
+  hungarian = document.getElementById('hungarian_boroughPark')
+  hungarian.style.opacity = '1'
+  italian = document.getElementById('italian_boroughPark')
+  italian.style.opacity = '1'
+  polish = document.getElementById('polish_boroughPark')
+  polish.style.opacity = '1'
+  russian = document.getElementById('russian_boroughPark')
+  russian.style.opacity = '1'
+  spanish = document.getElementById('spanish_boroughPark')
+  spanish.style.opacity = '1'
+  german = document.getElementById('german_boroughPark')
+  german.style.opacity = '1'
+  others = document.getElementById('others_boroughPark')
+  others.style.opacity = '1'
+  not_reported = document.getElementById('not_reported_boroughPark')
+  not_reported.style.opacity = '1'
+  yiddish = document.getElementById('yiddish_boroughPark')
+  yiddish.style.opacity = '1'
+})
